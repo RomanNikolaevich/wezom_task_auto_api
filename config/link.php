4a383f6010a1861d25c5bd67f25fa989bdef500b
Admin <roman.nikolaevich@gmail.com>
2023-03-03 12:33:37 +0200

    add config

---
+++ config/link.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    /**
+     * URL for obtaining data from NHTSA | National Highway Traffic Safety Administration
+     */
+    'api_url' => 'https://vpic.nhtsa.dot.gov/api/vehicles/',
+
+    'format' => '?format=json',
+
+
+];
