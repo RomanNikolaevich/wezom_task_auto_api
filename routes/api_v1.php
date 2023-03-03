c4b3c218a53cad614270d448db025f15848abdac
Admin <roman.nikolaevich@gmail.com>
2023-03-03 12:32:01 +0200

    union index + add Exception / error messages

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -4,9 +4,8 @@ use App\Http\Controllers\API\V1\CarAutocompleteController;
 use App\Http\Controllers\API\V1\StolenCarController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [StolenCarController::class, 'index'])->name('home');
 Route::post('cars', [StolenCarController::class, 'store'])->name('store');
-Route::get('cars', [StolenCarController::class, 'indexFiltered'])->name('indexFiltered');
+Route::get('cars', [StolenCarController::class, 'index'])->name('index');
 Route::get('cars/{export}', [StolenCarController::class, 'exportFiltered'])->name('exportFiltered');
 Route::put('cars/{vin}', [StolenCarController::class, 'update'])->name('update');
 Route::delete('cars/{vin}', [StolenCarController::class, 'destroy'])->name('destroy');
